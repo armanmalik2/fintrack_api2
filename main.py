e320487e58a705742dc77ca49e69e60cf113e8b6
armanmalik2 <armanmalik3370@gmail.com>
2025-05-01 08:31:40 +0530

    Add files via upload
    fintrack expense predictor

---
+++ main.py
@@ -0,0 +1,32 @@
+# main.py
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+import numpy as np
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# Enable CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Replace * with your frontend URL in production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Load your trained model
+model = joblib.load('expenses_ai.joblib')
+
+# Define the input schema
+class PredictionInput(BaseModel):
+    salary: float
+    year: int
+    month: int
+
+@app.post("/predict")
+def predict(data: PredictionInput):
+    features = np.array([[data.year, data.month, data.salary]])
+    prediction = model.predict(features)
+    return {"predicted_expenses": float(prediction[0])}
---
+++ predict_expense.py
@@ -0,0 +1,24 @@
+#CSV file se data Extraction in variable x and y
+import pandas as pd
+
+df = pd.read_csv('FinTrack_Expense_Salary_Data1.csv')
+
+x = df[['Year','Month','Salary']]
+y = df['Total_Expenses']
+
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+x_scaled = scaler.fit_transform(x)
+
+
+from sklearn.linear_model import LinearRegression
+
+model = LinearRegression()
+model.fit(x_scaled,y)
+run = model.predict(scaler.transform([[2140,10,200000]]))
+print(run)
+
+
+import joblib
+
+joblib.dump((model), 'expenses_ai.joblib')
